e591f8707dce0f24e04043d744dc353bf531bb57
MingyangLi-bit <mingyangli2024@u.northwestern.edu>
2024-05-06 09:24:50 -0500

    class code

--- recipe_step_tracker_api/types.py
+++ recipe_step_tracker_api/types.py
@@ -2,3 +2,16 @@ import strawberry_django
 from strawberry import auto, ID
 
 from .models import RecipeStep as RecipeStepModel
+
+@strawberry_django.type(RecipeStepModel)
+class RecipeStep:
+    id: ID
+    step: auto
+    completed: auto
+    
+@strawberry_django.input(RecipeStepModel)
+class RecipeStepInput:
+    id: ID = None
+    step: auto
+    completed: auto
+    
